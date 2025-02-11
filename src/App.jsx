d470129b9aefec4b0162eef2c951cf258df5edd1
Julia Obshansky <yuliaobs@gmail.com>
2025-02-10 22:43:05 -0500

    added audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Home,Picture,Card,Cake,Present } from "./components"
 import './index.css'
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const playMusic = () => {
+      const audio = document.getElementById("bg-music");
+      if (audio) {
+        audio.volume = 0.5;
+        audio.play().catch((error) => console.log("Autoplay blocked:", error));
+      }
+    };
+
+    document.addEventListener("click", playMusic);
+    return () => document.removeEventListener("click", playMusic);
+  }, []);
 
   return (
     <Router>
-      <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/pictures" element={<Picture/>}/>
-          <Route path="/card" element={<Card/>}/>
-          <Route path="/cake" element={<Cake/>}/>
-          <Route path="/present" element={<Present/>}/>
-      </Routes>
+      <div>
+        {/* 🎵 Background Music */}
+        <audio id="bg-music" loop>
+          <source src="/public/assets/Afternoon Jazz  [jazz lofi].mp3" type="audio/mpeg" />
+        </audio>
 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pictures" element={<Picture />} />
+          <Route path="/card" element={<Card />} />
+          <Route path="/cake" element={<Cake />} />
+          <Route path="/present" element={<Present />} />
+        </Routes>
+      </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
