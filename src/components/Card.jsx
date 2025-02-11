6715a8d81a69548b16cf6de52e86263f4b2cec2e
Julia Obshansky <yuliaobs@gmail.com>
2025-02-10 19:00:41 -0500

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -55,7 +55,7 @@ function Card() {
         <div id="card-front">
           <div className="wrap">
             <h1>С Днём Рождения Мамуля!</h1>
-            <img src={gifpicture} alt="Birthday Roses" className="w-max-70 max-h-70"></img>
+            <img src={gifpicture} alt="Birthday Roses" className="w-max-60 max-h-60"></img>
           </div>
         </div>
     </div>
